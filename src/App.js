49a0604fec1cd367e4054408f32bedcb31d2a32c
Rohit kumar <sun.rohit.kumar@gmail.com>
2021-05-12 00:32:01 +0530

    userState i react hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import ClassCounter from './componenets/ClassCounter';
 import HookCounter from './componenets/HookCounter';
+import HookCounterThree from './componenets/HookCounterThree';
+import HookCounterTwo from './componenets/HookCounterTwo';
+import HooksCounterFour from './componenets/HooksCounterFour';
 
 function App() {
   return (
     <div className="App">
-    <HookCounter />
+    <HooksCounterFour />
+    {/* <HookCounterThree /> */}
+    {/* <HookCounterTwo /> */}
+    {/* <HookCounter /> */}
       {/* <ClassCounter /> */}
     </div>
   );
