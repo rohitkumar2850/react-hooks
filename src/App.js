dd45cb64aee7d98ccc056ecb6ecdbf7e4e303963
Rohit kumar <sun.rohit.kumar@gmail.com>
2021-05-13 15:09:45 +0530

    userContext in Hook

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import './App.css';
-import ClassCounter from './componenets/ClassCounter';
-import HookCounter from './componenets/HookCounter';
-import HookCounterThree from './componenets/HookCounterThree';
-import HookCounterTwo from './componenets/HookCounterTwo';
-import HooksCounterFour from './componenets/HooksCounterFour';
-import HookCounterOne from './component_useEffect/HookCounterOne';
-import HookMouse from './component_useEffect/HookMouse';
-import IntervalHookCounter from './component_useEffect/IntervalHookCounter';
-import MouseContainer from './component_useEffect/MouseContainer';
-import DataFetching from './data_fetching_useEffect/DataFetching';
+import "./App.css";
+import React from "react";
+import ClassCounter from "./componenets/ClassCounter";
+import HookCounter from "./componenets/HookCounter";
+import HookCounterThree from "./componenets/HookCounterThree";
+import HookCounterTwo from "./componenets/HookCounterTwo";
+import HooksCounterFour from "./componenets/HooksCounterFour";
+import HookCounterOne from "./component_useEffect/HookCounterOne";
+import HookMouse from "./component_useEffect/HookMouse";
+import IntervalHookCounter from "./component_useEffect/IntervalHookCounter";
+import MouseContainer from "./component_useEffect/MouseContainer";
+import DataFetching from "./data_fetching_useEffect/DataFetching";
+import ComponentC from "./useContext_Hook/ComponentC";
+
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
-     <DataFetching />
+      <UserContext.Provider value={"Rohit"}>
+        <ChannelContext.Provider value={"Kumar"}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
 
-    {/* <IntervalHookCounter /> */}
-    {/* <MouseContainer /> */}
-    {/* <HookMouse /> */}
-    {/* <HookCounterOne /> */}
+      {/* <DataFetching /> */}
 
+      {/* <IntervalHookCounter /> */}
+      {/* <MouseContainer /> */}
+      {/* <HookMouse /> */}
+      {/* <HookCounterOne /> */}
 
-    {/* <HooksCounterFour /> */}
-    {/* <HookCounterThree /> */}
-    {/* <HookCounterTwo /> */}
-    {/* <HookCounter /> */}
+      {/* <HooksCounterFour /> */}
+      {/* <HookCounterThree /> */}
+      {/* <HookCounterTwo /> */}
+      {/* <HookCounter /> */}
       {/* <ClassCounter /> */}
     </div>
   );
