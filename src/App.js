8ee7d2c8f0cb5936388f797eeb5508d80b26a908
Rohit kumar <sun.rohit.kumar@gmail.com>
2021-05-13 23:23:29 +0530

    Fetching data with useReducer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import "./App.css";
 import React, { useReducer } from "react";
-// import ClassCounter from "./componenets/ClassCounter";
-// import HookCounter from "./componenets/HookCounter";
-// import HookCounterThree from "./componenets/HookCounterThree";
-// import HookCounterTwo from "./componenets/HookCounterTwo";
-// import HooksCounterFour from "./componenets/HooksCounterFour";
-// import HookCounterOne from "./component_useEffect/HookCounterOne";
-// import HookMouse from "./component_useEffect/HookMouse";
-// import IntervalHookCounter from "./component_useEffect/IntervalHookCounter";
-// import MouseContainer from "./component_useEffect/MouseContainer";
-// import DataFetching from "./data_fetching_useEffect/DataFetching";
-// import ComponentC from "./useContext_Hook/ComponentC";
-// import CounterOne from "./useReducer_hook/CounterOne";
-// import CounterTwo from "./useReducer_hook/CounterTwo";
-// import CounterThree from "./useReducer_hook/CounterThree";
+import ClassCounter from "./componenets/ClassCounter";
+import HookCounter from "./componenets/HookCounter";
+import HookCounterThree from "./componenets/HookCounterThree";
+import HookCounterTwo from "./componenets/HookCounterTwo";
+import HooksCounterFour from "./componenets/HooksCounterFour";
+import HookCounterOne from "./component_useEffect/HookCounterOne";
+import HookMouse from "./component_useEffect/HookMouse";
+import IntervalHookCounter from "./component_useEffect/IntervalHookCounter";
+import MouseContainer from "./component_useEffect/MouseContainer";
+import DataFetching from "./data_fetching_useEffect/DataFetching";
+//import ComponentC from "./useContext_Hook/ComponentC";
+import CounterOne from "./useReducer_hook/CounterOne";
+import CounterTwo from "./useReducer_hook/CounterTwo";
+import CounterThree from "./useReducer_hook/CounterThree";
 import ComponentA from "./useReducer_with_useContext/ComponentA";
 import ComponentB from "./useReducer_with_useContext/ComponentB";
 import ComponentC from "./useReducer_with_useContext/ComponentC";
+import DataFetchingOne from "./fetching_data_with_useReducer/DataFetchingOne";
+import DataFetchingTwo from "./fetching_data_with_useReducer/DataFetchingTwo";
 // export const UserContext = React.createContext();
 // export const ChannelContext = React.createContext();
 
@@ -36,69 +38,78 @@ const reducer = (state, action) => {
   }
 };
 
-function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
+function App(){
   return (
-    <CountContext.Provider
-      value={{ countState: count, countDispatch: dispatch }}>
-      <div className="App">
-        Count - {count}
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
-      </div>
-    </CountContext.Provider>
-  );
-  {
-    /* <CounterThree /> */
-  }
-  {
-    /* <CounterTwo /> */
-  }
-  {
-    /* <CounterOne /> */
-  }
-  {
-    /* <UserContext.Provider value={"Rohit"}>
-        <ChannelContext.Provider value={"Kumar"}>
-          <ComponentC />
-        </ChannelContext.Provider>
-      </UserContext.Provider> */
-  }
-  {
-    /* <DataFetching /> */
-  }
-  {
-    /* <IntervalHookCounter /> */
-  }
-  {
-    /* <MouseContainer /> */
-  }
-  {
-    /* <HookMouse /> */
-  }
-  {
-    /* <HookCounterOne /> */
-  }
-  {
-    /* <HooksCounterFour /> */
-  }
-  {
-    /* <HookCounterThree /> */
-  }
-  {
-    /* <HookCounterTwo /> */
-  }
-  {
-    /* <HookCounter /> */
-  }
-  {
-    /* <ClassCounter /> */
-  }
-  {
-    /* </div> */
-  }
-  // );
+    <div>
+    <DataFetchingTwo />
+    {/* <DataFetchingOne /> */}
+    </div>
+  )
 }
+
+// function App() {
+//   const [count, dispatch] = useReducer(reducer, initialState);
+//   return (
+//     <CountContext.Provider
+//       value={{ countState: count, countDispatch: dispatch }}>
+//       <div className="App">
+//         Count - {count}
+//         <ComponentA />
+//         <ComponentB />
+//         <ComponentC />
+//       </div>
+//     </CountContext.Provider>
+//   );
+//   {
+//     /* <CounterThree /> */
+//   }
+//   {
+//     /* <CounterTwo /> */
+//   }
+//   {
+//     /* <CounterOne /> */
+//   }
+//   {
+//     /* <UserContext.Provider value={"Rohit"}>
+//         <ChannelContext.Provider value={"Kumar"}>
+//           <ComponentC />
+//         </ChannelContext.Provider>
+//       </UserContext.Provider> */
+//   }
+//   {
+//     /* <DataFetching /> */
+//   }
+//   {
+//     /* <IntervalHookCounter /> */
+//   }
+//   {
+//     /* <MouseContainer /> */
+//   }
+//   {
+//     /* <HookMouse /> */
+//   }
+//   {
+//     /* <HookCounterOne /> */
+//   }
+//   {
+//     /* <HooksCounterFour /> */
+//   }
+//   {
+//     /* <HookCounterThree /> */
+//   }
+//   {
+//     /* <HookCounterTwo /> */
+//   }
+//   {
+//     /* <HookCounter /> */
+//   }
+//   {
+//     /* <ClassCounter /> */
+//   }
+//   {
+//     /* </div> */
+//   }
+//   // );
+// }
 
 export default App;
